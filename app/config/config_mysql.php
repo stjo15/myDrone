c788089889437466e1ec840d805d7eaaffbb8d76
Staffan Johansson <stalle.johansson@gmail.com>
2015-12-02 15:33:54 +0100

    added db config file

--- app/config/config_mysql.php
+++ app/config/config_mysql.php
@@ -2,12 +2,12 @@
 
 // Settings for private server
 
-define('DB_USER', 'root'); // The database username
-define('DB_PASSWORD', 'Juhee27!'); // The database password
+define('DB_USER', 'myusername'); // The database username
+define('DB_PASSWORD', 'mypassword!'); // The database password
 
 return [
     // Set up details on how to connect to the database
-    'dsn'     => "mysql:host=localhost;dbname=mesidan;",
+    'dsn'     => "mysql:host=localhost;dbname=mydbname;",
     'username'        => DB_USER,
     'password'        => DB_PASSWORD,
     'driver_options'  => [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"],
@@ -19,25 +19,3 @@ return [
     // Throw a more verbose exception when failing to connect
     'debug_connect' => 'false',
 ];
-
-// Settings for BTH server
-/*
-define('DB_USER', 'yourUsername');
-define('DB_PASSWORD', 'yourPassword'); 
-
-return [
-    // Set up details on how to connect to the database
-    'dsn'     => "mysql:host=yourHost;dbname=yourDatabaseName;",
-    'username'        => DB_USER,
-    'password'        => DB_PASSWORD,
-    'driver_options'  => [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"],
-    'table_prefix'    => "drone_",
-
-    // Display details on what happens
-    'verbose' => false,
-
-    // Throw a more verbose exception when failing to connect
-    'debug_connect' => 'false',
-];
-
-*/
